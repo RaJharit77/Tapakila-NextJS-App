2cbd1daef464d803758ec06fff1b4995f927584f
salomia <salomia.razakarivony@gmail.com>
2025-03-27 10:53:25 +0300

    fixed: prisma

--- prisma/migrations/20250306114002_update_ticket_model/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `ticket_available_number` on the `Ticket` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Ticket" DROP COLUMN "ticket_available_number";
